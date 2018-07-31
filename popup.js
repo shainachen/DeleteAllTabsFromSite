95af3d89ebdb90d199eaea749efd1272225cee1d
shainachen <shaina.sierra@gmail.com>
2018-07-31 14:11:34 -0700

    add delete current tab on click of popup button

--- popup.js
+++ popup.js
@@ -1,9 +1,16 @@
 let deleteTabs = document.getElementById('deleteTabsFromSite');
 
-deleteTabs.onclick = function(tabs) {
-	alert('The button action was clicked! Yay!');
-)
-}
+deleteTabs.onclick = function(tab) {
+	console.log("clicked on popup");
+	chrome.tabs.query({active: true}, function(tabs){
+	chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: chrome.tabs.remove(tabs[0].id)}
+);
+	});
+};
+
+
 /*    chrome.tabs.remove(tab.id, function(){
       appendToLog('tab: ' + tabId + ' removed.');
      });
