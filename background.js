e71546a3b1e1aaf224db7daf8e8cdc9e1cfb2ed7
shainachen <shaina.sierra@gmail.com>
2018-08-01 10:19:42 -0700

    remove background

--- background.js
+++ background.js
@@ -1,6 +1,1 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.remove(tab.id);
 
-  // Okay, the actual action should go here
-  // And look, we already have the required Tab object for free!
-});
